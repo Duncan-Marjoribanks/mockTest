5e8177e2eed26b797465d91cf695f184772bc571
Duncan <marjoribanks.d@gmail.com>
2018-06-11 15:24:33 +0100

    item test file created and tests written for getters and setter for price

---
+++ src/test/java/ItemTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ItemTest {
+
+    Item item;
+
+    @Before
+    public void before() {
+        item = new Item("item", 50.00);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("item", item.getName());
+    }
+
+    @Test
+    public void canGetPrice() {
+        assertEquals(50.00, item.getPrice());
+    }
+
+    @Test
+    public void canSetPrice() {
+        item.setPrice(25.00);
+        assertEquals(25.00, item.getPrice());
+    }
+    
+}
