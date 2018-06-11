44b66735cc28cd926fb09303f4c13a8f9033d183
Duncan <marjoribanks.d@gmail.com>
2018-06-11 15:36:33 +0100

    added basket test file with tests for add remove empty and totalCost functions

---
+++ src/test/java/BasketTest.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BasketTest {
+
+    Basket basket;
+    Item item;
+
+    @Before
+    public void before() {
+        basket = new Basket();
+        item = new Item("thing", 50.00);
+    }
+
+    @Test
+    public void canCountBasket() {
+        assertEquals(0, basket.countItems());
+    }
+
+    @Test
+    public void canAddItem() {
+        basket.addItem(item);
+        assertEquals(1, basket.countItems());
+    }
+
+    @Test
+    public void canremoveItem() {
+        basket.addItem(item);
+        basket.removeItem(item);
+        assertEquals(0, basket.countItems());
+    }
+
+    @Test
+    public void canClearBasket() {
+        basket.addItem(item);
+        basket.addItem(item);
+        basket.clearItems();
+        assertEquals(0, basket.countItems());
+    }
+
+    @Test
+    public void canGetTotalCostOfAllItems() {
+        basket.addItem(item);
+        basket.addItem(item);
+        assertEquals(100.00, basket.getTotalCost(), 0.01);
+    }
+}
